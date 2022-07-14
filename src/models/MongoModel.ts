44509e0546c00a5dcb7dd50077bf122ed7809ea2
mabiiak <mabiane.polniak@gmail.com>
2022-07-13 23:16:49 -0300

    fix: generic model

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,33 @@
+import { Model, isValidObjectId } from 'mongoose';
+import { Model as IModel } from '../interfaces/ModelInterface';
+
+abstract class MongoModel<T> implements IModel<T> {
+  constructor(protected model: Model<T>) {}
+
+  async create(obj: T): Promise<T> {
+    const created = await this.model.create({ ...obj });
+    return created;
+  }
+
+  async read(): Promise<T[]> {
+    const itemList = await this.model.find();
+    return itemList;
+  }
+
+  async readOne(id: string): Promise<T | null> {
+    const itemId = await this.model.findById({ _id: id });
+    return itemId;
+  }
+
+  async update(id: string, obj: T): Promise<T | null> {
+    if (!isValidObjectId(id)) return null;
+    return this.model.findOneAndUpdate({ _id: id }, { ...obj });
+  }
+
+  async delete(id: string): Promise<T | null> {
+    if (!isValidObjectId(id)) return null;
+    return this.model.findOneAndDelete({ _id: id });
+  }
+}
+
+export default MongoModel;
