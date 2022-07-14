d95285f3c9fde7416cf4f5193c23fdd5a63b7220
mabiiak <mabiane.polniak@gmail.com>
2022-07-14 12:14:38 -0300

    feat: create validantion error id's

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -6,8 +6,14 @@ import ServiceInterface from '../interfaces/ServiceInterface';
 class CarsController implements ControllerInterface {
   private _carService: ServiceInterface;
 
+  errorNotFount: string;
+
+  errorLength: string;
+
   constructor(carService: ServiceInterface = new CarsService()) {
     this._carService = carService;
+    this.errorNotFount = 'Object not found';
+    this.errorLength = 'Id must have 24 hexadecimal characters';
   }
 
   async create(
@@ -45,7 +51,16 @@ class CarsController implements ControllerInterface {
   ): Promise<void | Response> {
     try {
       const { id } = req.params;
+
+      if (id.length < 23) {
+        return res.status(400).json({ error: this.errorLength });
+      }
+
       const oneCar = await this._carService.readOne(id);
+
+      if (oneCar === null) {
+        return res.status(404).json({ error: this.errorNotFount });
+      }
 
       return res.status(200).json(oneCar);
     } catch (err) {
@@ -60,9 +75,16 @@ class CarsController implements ControllerInterface {
   ): Promise<Response | void> {
     try {
       const { id } = req.params;
+      if (id.length < 23) {
+        return res.status(400).json({ error: this.errorLength });
+      }
+
       const itemUpdated = await this._carService.update(id, req.body);
 
-      return res.status(201).json(itemUpdated);
+      if (itemUpdated === null) {
+        return res.status(404).json({ error: this.errorNotFount });
+      }
+      return res.status(200).json(itemUpdated);
     } catch (err) {
       next(err);
     }
@@ -75,7 +97,17 @@ class CarsController implements ControllerInterface {
   ): Promise<Response | void> {
     try {
       const { id } = req.params;
-      await this._carService.update(id, req.body);
+      
+      if (id.length < 23) { 
+        return res.status(400)
+          .json({ error: 'Id must have 24 hexadecimal characters' });
+      }
+
+      const deleteItem = await this._carService.delete(id);
+      
+      if (!deleteItem) {
+        return res.status(404).json({ error: this.errorNotFount });
+      }
 
       return res.status(204).end();
     } catch (err) {
