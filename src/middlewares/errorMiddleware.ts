6905284d728607ecda7d6ccebd823962512cf223
mabiiak <mabiane.polniak@gmail.com>
2022-07-14 12:07:37 -0300

    fix: middleware error

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,17 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { ZodError } from 'zod';
+import IError from '../interfaces/IError';
 
 function errorMiddleware(
-  err: unknown,
+  err: unknown | IError,
   _req: Request,
   res: Response,
-  next: NextFunction,
-) {
+  _next: NextFunction,
+) {  
   if (err instanceof ZodError) {
     return res.status(400).json({ message: err.message });
   }
-  console.log(err);
-  return res.status(500).json({ message: 'internal error' });
+
+  res.status(500).json({ message: 'internal error' });
 }
 
 export default errorMiddleware;
